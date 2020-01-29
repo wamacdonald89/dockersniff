47884495d4df4087fc419713b087e6b7dba7a8d5
Andy <wamacdonald89@gmail.com>
2020-01-29 18:20:39 -0500

    fixed

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,7 @@
 ip link set ${IFACE} down
-iw dev wlp2s0 set monitor control
+iw dev ${IFACE} set monitor control
 ip link set ${IFACE}  up
-iw dev wlp2s0 set channel ${DS_CHANNEL} 
+iw dev ${IFACE} set channel ${DS_CHANNEL} 
 cd /opt/dockersniff/
 /usr/bin/python3 snift.py ${IFACE}
 /bin/bash
