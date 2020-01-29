2e694a7aa0972c1c3e740ec3f98b9f7e3cf082a4
Andy <wamacdonald89@gmail.com>
2020-01-29 18:14:18 -0500

    hardcoded values are bad

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
-ip link set wlp2s0 down
+ip link set ${IFACE} down
 iw dev wlp2s0 set monitor control
-ip link set wlp2s0 up
+ip link set ${IFACE}  up
 iw dev wlp2s0 set channel ${DS_CHANNEL} 
 cd /opt/dockersniff/
 /usr/bin/python3 snift.py ${IFACE}
